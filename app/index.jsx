044e7f87dd80df1286b8f28e62872f0b6d647a49
Little Jacob <joelnoumbi28@gmail.com>
2023-12-07 10:21:01 +0100

    home + parallax

--- app/index.jsx
+++ app/index.jsx
@@ -6,8 +6,10 @@ import {
 } from "react-native-responsive-screen";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated, { FadeIn, FadeInDown, FadeOut } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View className="flex-1 justify-end">
       <StatusBar style="light" />
@@ -43,6 +45,7 @@ export default function Index() {
 
         <Animated.View entering={FadeInDown.delay(200).springify()}>
           <TouchableOpacity
+            onPress={() => router.push("home")}
             style={{ height: hp(7), width: wp(80) }}
             className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
           >
